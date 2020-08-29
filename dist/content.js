c08c52529854508de679c8fa2395280d45fd426b
T Arun <arunthulasiraman@gmail.com>
2020-08-29 23:08:31 +0530

    main.js and content.js communication working

--- dist/content.js
+++ dist/content.js
@@ -1,1 +1,16 @@
+"use strict";
+
+(function(){
+  if (window.hasRun){
+    return;
+  }
+  window.hasRun = true;
+
+  browser.runtime.onMessage.addListener((message) =>{
+    console.log('hi')
+    const {joinTime, leaveTime} = message;
+    console.log(joinTime, leaveTime)
+  })
+})();
+
 console.log('contentscript')
--- pop/main.js
+++ pop/main.js
@@ -1,5 +1,4 @@
 import { Timepicker } from "materialize-css";
-import $ from "jquery";
 import onInit from './oninit'
 
 import "materialize-css/dist/css/materialize.min.css";
@@ -10,16 +9,3 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = Timepicker.init(elems);
 });
 
-console.log('main.js')
-
-document.getElementById('submit').addEventListener('click', () => {
-  console.log('popup loaded')
-})
-
-// let submit = document.getElementsByClassName("submit")[0];
-// submit.addEventListener("click", () => {
-//   document.getElementsByClassName("datepicker")[0].click();
-// });
-// onInit()
-
-// document.addEventListener("DOMContentLoaded", onInit, false);
