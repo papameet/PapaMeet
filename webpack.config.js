6d158f58bd0a15546a12a9312b78b150dba94089
T Arun <arunthulasiraman@gmail.com>
2020-09-09 00:21:42 +0530

    Webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
 const path = require("path");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
     main: "./src/main.js",
-    content: "./src/content.js"
+    content: "./src/content.js",
   },
   mode: "development",
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader'}, "css-loader"],
+        use: [{ loader: "style-loader" }, "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
       },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "static"),
+          to: path.resolve(__dirname, "dist"),
+        }
+      ]
+    })
+  ]
 };
